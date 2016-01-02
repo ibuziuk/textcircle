ba262d7f8c39b644cbb569a283c46e066826a4ce
Ilya Buziuk <ibuziuk@exadel.com>
2016-01-03 00:58:13 +0300

    Week-4: Final implementation

--- client/accounts.js
+++ client/accounts.js
@@ -1,8 +1,6 @@
 
 
-// configure the special accounts user interface
-// by setting up some extra fields and specifying constraints
-// see:https://github.com/ianmartorell/meteor-accounts-ui-bootstrap-3/    
+        
 Accounts.ui.config({
     requestPermissions: {},
     extraSignupFields: [{
